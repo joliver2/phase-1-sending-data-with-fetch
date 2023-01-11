18f8391fe03f83aa7db13602093903e24452ba64
joliver427@icloud.com <joliver427@icloud.com>
2023-01-10 21:00:45 -0700

    commit tree 4942261b15309987c81285e4f77807d2136f8ff9

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(response => response.json())
+    .then(function (object) {
+        const p = document.createElement('p')
+        p.innerHTML = object.id
+        document.body.appendChild(p)
+    })
+    .catch(function (message = "ALERT!") {
+        const error = document.createElement('p')
+        error.innerHTML = message
+        document.body.appendChild(error)
+    })
+}
+
+submitData()
